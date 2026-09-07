11bb464e5674e61c7e38ae0697336984a51c9a8c
cruizba <7049047+cruizba@users.noreply.github.com>
2026-09-07 13:35:11 +0000

    chore: update third-party versions

--- pro/ha/tf-do-openvidu-ha.tf
+++ pro/ha/tf-do-openvidu-ha.tf
@@ -1680,8 +1680,8 @@ CONFIG_S3_EOF
   lsb-release \
   openssl
 
-  AWS_CLI_VERSION=2.35.5
-  DOCTL_VERSION=1.162.0
+  AWS_CLI_VERSION=2.36.40
+  DOCTL_VERSION=1.168.0
 
   install_aws_cli() {
     curl -fsSL "https://awscli.amazonaws.com/awscli-exe-linux-$(uname -m)-$${AWS_CLI_VERSION}.zip" -o /tmp/awscliv2.zip
@@ -1979,8 +1979,8 @@ apt-get update && apt-get install -y \
   lsb-release \
   openssl
 
-AWS_CLI_VERSION=2.35.5
-DOCTL_VERSION=1.162.0
+AWS_CLI_VERSION=2.36.40
+DOCTL_VERSION=1.168.0
 
 install_aws_cli() {
   curl -fsSL "https://awscli.amazonaws.com/awscli-exe-linux-$(uname -m)-$${AWS_CLI_VERSION}.zip" -o /tmp/awscliv2.zip
